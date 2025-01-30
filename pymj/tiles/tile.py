b27f71e4561a9916e73c4e708b62df451d95aa92
Taeho <taehogwon96@gmail.com>
2025-01-30 10:20:35 +0900

    add tile mapping docstring

--- pymj/tiles/tile.py
+++ pymj/tiles/tile.py
@@ -13,22 +13,48 @@ class Tile:
     Attributes:
         tile_type (TileType): The type or suit of the Mahjong tile
             (e.g., man, pin, sou, wind, dragon, etc.(like flower).
-        value (int): The numerical value of the tile within its type
-            (typically ranges from 1 to 9 for number tiles,
-            wind : east = 1, south = 2, west = 3, north = 4,
-            dragon : white = 1, green = 2, red = 3).
+        value (int): The numerical value of the tile within its type:
+            - Number tiles (MAN/PIN/SOU): Values from 1 to 9
+            - Wind tiles: East(1), South(2), West(3), North(4)
+            - Dragon tiles: White(1), Green(2), Red(3)
+            - ETC types: special tiles
+    Raises:
+        ValueError: If the provided value is outside the valid range for the
+                   specified tile type. For example:
+                   - Values > 9 for number tiles (MAN/PIN/SOU)
+                   - Values > 4 for wind tiles
+                   - Values > 3 for dragon tiles
 
-    Example:
-        # Creating a tile representing the 3pin
-        tile = Tile(tile_type=TileType.PIN, value=3)
+    Examples:
+        Creating a Five of Characters (5 Man):
+        >>> five_man = Tile(tile_type=TileType.MAN, value=5)
 
-        # Creating a tile representing the East wind
-        wind_tile = Tile(tile_type=TileType.WIND, value=1)
+        Creating an East Wind tile:
+        >>> east_wind = Tile(tile_type=TileType.WIND, value=1)
+
+        Creating a White Dragon tile:
+        >>> white_dragon = Tile(tile_type=TileType.DRAGON, value=1)
+
+        This will raise a ValueError (invalid value for PIN type):
+        >>> invalid_tile = Tile(tile_type=TileType.PIN, value=10)
 
     """
 
     tile_type: TileType
     value: int
+
+    def __post_init__(self) -> None:
+        max_tile_map = {
+            TileType.MAN: 9,
+            TileType.PIN: 9,
+            TileType.SOU: 9,
+            TileType.WIND: 4,
+            TileType.DRAGON: 3,
+        }
+        if self.tile_type is not TileType.ETC and not (
+            1 <= self.value <= max_tile_map[self.tile_type]
+        ):
+            raise ValueError
 
     def __repr__(self) -> str:
         return f"{self.tile_type.name} {self.value}"
--- pymj/tiles/tile_mapping.py
+++ pymj/tiles/tile_mapping.py
@@ -3,11 +3,44 @@ from pymj.tiles.tile import Tile
 
 
 class TileMapping:
-    """Handles mapping between tiles and indices."""
+    """Maps mahjong tiles to numerical indices and vice versa.
+
+    This class provides static methods to convert
+    between Tile objects and their corresponding numerical indices (0-33).
+
+    The mapping follows this pattern:
+    - 0-8   : Man
+    - 9-17  : Pin
+    - 18-26 : Sou
+    - 27-30 : Wind
+    - 31-33 : Dragon
+
+    The class ensures consistent mapping across the entire mahjong game implementation.
+    """
 
     @staticmethod
     def tile_to_index(tile: Tile) -> int:
-        """Convert a tile to its unique index."""
+        """Convert a mahjong tile to its unique numerical index.
+
+        Args:
+            tile (Tile): A Tile object representing a mahjong tile.
+
+        Returns:
+            int: The unique index (0-33) corresponding to the input tile.
+                - Man                          : 1-9 → 0-8
+                - Pin                          : 1-9 → 9-17
+                - Sou                          : 1-9 → 18-26
+                - Wind (East/South/West/North) : 1-4 → 27-30
+                - Dragon (White/Green/Red)     : 1-3 → 31-33
+
+
+        Raises:
+            ValueError: If the type of the tile is ETC
+
+        """
+        if tile.tile_type is TileType.ETC:
+            raise ValueError
+
         base_index = {
             TileType.MAN: 0,
             TileType.PIN: 9,
@@ -19,10 +52,18 @@ class TileMapping:
 
     @staticmethod
     def index_to_tile(index: int) -> Tile:
-        """Convert an index to its corresponding tile."""
-        if not 0 <= index < 34:
-            raise ValueError
+        """Convert a numerical index back to its corresponding mahjong tile.
 
+        Args:
+            index (int): The numerical index (0-33) of a mahjong tile.
+
+        Returns:
+            Tile: The Tile object corresponding to the input index.
+
+        Raises:
+            ValueError: If the index is not within the valid range [0, 33].
+
+        """
         if 0 <= index <= 8:
             return Tile(TileType.MAN, index + 1)
         elif 9 <= index <= 17:
@@ -31,5 +72,7 @@ class TileMapping:
             return Tile(TileType.SOU, index - 17)
         elif 27 <= index <= 30:
             return Tile(TileType.WIND, index - 26)
-        else:  # 31-33
+        elif 31 <= index <= 33:
             return Tile(TileType.DRAGON, index - 30)
+        else:
+            raise ValueError
